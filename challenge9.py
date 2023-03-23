5ddc761afdce48cbebff317f02f90151b73b7746
spencymitch <spencermitchell409@gmail.com>
2023-03-23 22:11:49 +0000

    challenge 9

---
+++ challenge9.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 09 Ops Challenge
+# Author: Spencer Mitchell
+# Date of latest revision: 3/23/23
+# Purpose: Create a Python script that includes the following:
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+a = 1
+b = 2
+
+if a == b:
+    print("a is equal to b")
+    
+elif a > b:
+    print("a is greater than b")
+elif a !=b:
+    print("a does not equal b")
+elif a < b:
+    print("a is less than b")
+elif a <= b:
+    print("a is less than or equal to b")
+elif a >= b:
+    print("a is greater than or equal to b")
+else: 
+    print ("b is greater than a")
+
+
