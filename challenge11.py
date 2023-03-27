5faf6020c15de794251ecf5341a8752b70cc6085
spencymitch <spencermitchell409@gmail.com>
2023-03-27 22:04:29 +0000

    saved purpose/resources

--- challenge11.py
+++ challenge11.py
@@ -3,8 +3,10 @@
 # Script: Ops 301 Class 11 Ops Challenge
 # Author: Spencer Mitchell
 # Date of latest revision: 3/27/23
-# Purpose:
+# Purpose: Create a Python script that fetches this information using Psutil:
+# Resource: Classmate Sierra helped me with some of this code
 
+# install psutil
 import psutil
 
 # Generate CPU times as a named tuple
@@ -12,22 +14,32 @@ print(psutil.cpu_times())
 
 # Show current CPU consumption
 print(psutil.cpu_percent())
+
 # Time spent by normal processes executing in user mode
 print(psutil.cpu_times().user)
+
 # Time spent by processes executing in kernel mode
 print(psutil.cpu_times().system)
+
 # Time when system was idle
 print(psutil.cpu_times().idle)
+
 # Time spent by priority processes executing in user mode
 print(psutil.cpu_times().nice)
+
 # Time spent waiting for I/O to complete.
 print(psutil.cpu_times().iowait)
+
 # Time spent for servicing hardware interrupts.
 print(psutil.cpu_times().irq)
+
 # Time spent for servicing software interrupts.
 print(psutil.cpu_times().softirq)
+
 # Time spent by other operating systems running in a virtualized environment
 print(psutil.cpu_times().steal)
+
 # Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
 print(psutil.cpu_times().guest)
+
 # end
